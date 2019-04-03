4f90467ce1404e52d55f46b32b1bfd007da15362
Martina <martina@rivizzigno.it>
2019-04-03 11:45:26 +0200

    set defaults paramters to invalid and check their validity before using

--- local_planner/src/nodes/local_planner.cpp
+++ local_planner/src/nodes/local_planner.cpp
@@ -475,7 +475,7 @@ avoidanceOutput LocalPlanner::getAvoidanceOutput() const {
 
   out.obstacle_ahead = obstacle_;
   out.velocity_around_obstacles = velocity_around_obstacles_;
-  out.velocity_far_from_obstacles = px4_.param_mpc_xy_cruise;
+  out.velocity_far_from_obstacles = std::isfinite(px4_.param_mpc_xy_cruise) ? px4_.param_mpc_xy_cruise : 3.f;
   out.last_path_time = last_path_time_;
 
   out.back_off_point = back_off_point_;
